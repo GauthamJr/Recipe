056051c9b90bb3a4b5c78b47e75c7a7975f4b67d
GauthamJr <100911561+GauthamJr@users.noreply.github.com>
2025-02-26 20:00:12 +0530

    Update vite.config.js

--- src/main/resources/static/Recipe-Frontend/vite.config.js
+++ src/main/resources/static/Recipe-Frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
